cd7a167074405d74d04dd8b8b607a3f8100adfe3
Nikica Novakovic <novakovicnikica@gmail.com>
2020-06-29 23:09:58 +0200

    Use env variables

--- App.ts
+++ App.ts
@@ -2,10 +2,13 @@ import { Application } from "https://deno.land/x/oak@v4.0.0/mod.ts";
 import router from "./Routes.ts";
 
 const app = new Application();
-const port = 9001;
+
+const env = Deno.env.toObject();
+const port = Number(env.PORT) || 9001;
+const host = env.HOST || "127.0.0.1";
 
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-console.log(`Server started on ${port}`);
+console.log(`Server started on ${host}:${port}`);
 await app.listen({ port });
